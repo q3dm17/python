af52d4f29ab192f09ace55346bffae6c5260738c
S.Rozhin <S.Rozhin@skbkontur.ru>
2015-07-14 09:32:37 +0500

    func scripts

---
+++ func.py
@@ -0,0 +1,16 @@
+﻿import random
+
+names = ['Masha', 'Petya', 'Vasia']
+code_names = ['Jokie', 'Vintik', 'Funtic']
+func_secret_names = map(lambda x: code_names[abs(hash(x))%len(code_names)], names)
+
+for i in range(len(names)):
+    names[i] = random.choice(code_names)
+print "names"
+print names
+print "func_secret_names"
+print func_secret_names
+
+
+joiner = reduce(lambda a,x: a + x, [0,1,2,3,4],12)
+print joiner
---
+++ func_2.py
@@ -0,0 +1,68 @@
+﻿people = [
+    {'name': 'Masha', 'h': 160},
+    {'h': 'Sasha', 'h': 80},
+    {'name': 'Pasha'}]
+hs = map(lambda dict: dict['h'], filter(lambda x: 'h' in x, people))
+# print reduce(lambda a, x: x+a, hs) / len(hs)
+
+
+def zero(s):
+    if s[0] == "0":
+        return s[1:]
+
+
+def one(s):
+    if s[0] == "1":
+        return s[1:]
+
+
+def rule_sequence(s, rules):
+    if s is None:
+        return None
+    elif len(rules) == 0:
+        return s
+    else:
+        return rule_sequence(rules[0](s), rules[1:])
+
+
+print rule_sequence('0101', [zero, one, zero])
+# => 1
+
+print rule_sequence('0101', [zero, zero])
+# => None
+
+
+def pipeline_each(data, funcs):
+    return reduce(lambda a, f: map(f, a), funcs, data)
+
+
+def repls(str):
+    return len(str)
+
+
+def ml(a):
+    return a * 2
+
+for s in pipeline_each(["aa", "bb"], [repls, ml]):
+    print s
+
+
+def adddd(_d, key, value):
+    from copy import deepcopy
+    d = deepcopy(_d)
+    d[key] = value
+    return d
+
+def assoc(_d, keys):
+    from copy import deepcopy
+    return reduce(lambda a, x: adddd(a, x, _d[x]), keys, {})
+
+def call(fn, key):
+    def apply_fn(record):
+        return assoc(record, key, fn(record.get(key)))
+    return apply_fn
+
+def pluck(fields):
+    def extract_fn(dict):
+        return assoc(dict, fields)
+    return extract_fn
